e3451035a3f124a6857388c36b65e657978032f8
bsametarman@outlook.com <bsametarman@outlook.com>
2022-05-14 18:39:35 +0300

    service classes added

---
+++ src/main/java/com/example/hrms/business/abstracts/ResumeService.java
@@ -0,0 +1,12 @@
+package com.example.hrms.business.abstracts;
+
+import java.util.List;
+
+import com.example.hrms.core.utilities.results.DataResult;
+import com.example.hrms.entities.concretes.Resume;
+
+public interface ResumeService {
+	DataResult<List<Resume>> getAll();
+	DataResult<Resume> add(Resume resume);
+	
+}
---
+++ src/main/java/com/example/hrms/business/concretes/ResumeManager.java
@@ -0,0 +1,33 @@
+package com.example.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.hrms.business.abstracts.ResumeService;
+import com.example.hrms.core.utilities.results.DataResult;
+import com.example.hrms.core.utilities.results.SuccessDataResult;
+import com.example.hrms.dataAccess.abstracts.ResumeDao;
+import com.example.hrms.entities.concretes.Resume;
+
+@Service
+public class ResumeManager implements ResumeService{
+	
+	private ResumeDao resumeDao;
+	
+	public ResumeManager(ResumeDao resumeDao) {
+		super();
+		this.resumeDao = resumeDao;
+	}
+	
+	@Override
+	public DataResult<List<Resume>> getAll() {
+		return new SuccessDataResult<List<Resume>>(this.resumeDao.findAll(), "Tüm CV'ler listelendi!");
+	}
+
+	@Override
+	public DataResult<Resume> add(Resume resume) {
+		return new SuccessDataResult<Resume>(this.resumeDao.save(resume), "CV eklendi!");
+	}
+
+}
