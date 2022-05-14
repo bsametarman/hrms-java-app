910438bae5df7dc589d41924a6846ee805ae6836
bsametarman@outlook.com <bsametarman@outlook.com>
2022-05-14 17:57:35 +0300

    resume entity added

---
+++ src/main/java/com/example/hrms/entities/concretes/Resume.java
@@ -0,0 +1,52 @@
+package com.example.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.example.hrms.entities.abstracts.IEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="resumes")
+public class Resume implements IEntity{
+	
+	@Id
+	@GeneratedValue
+	@Column(name="resume_id")
+	private int resumeId;
+	
+	@Column(name="resume_name")
+	private String resumeName;
+	
+	@Column(name="employee_name")
+	private String employeeName;
+	
+	@Column(name="employee_surname")
+	private String employeeSurname;
+	
+	@Column(name="employee_age")
+	private int employeeAge;
+	
+	@Column(name="employee_gender")
+	private String employeeGender;
+	
+	@Column(name="employee_nationality")
+	private String employeeNationality;
+	
+	@Column(name="employee_profile_image")
+	private String employeeProfileImage;
+	
+	
+	
+	
+	
+}
